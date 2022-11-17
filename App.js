c4a4ddb65ef045d96fc915cfcfe053c630aa7d42
chan <mo0om487@naver.com>
2022-11-17 15:18:46 +0900

    test jy

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>mumamoma</Text>
+      <Text>mumamoma team</Text>
       <StatusBar style="auto" />
     </View>
   );
