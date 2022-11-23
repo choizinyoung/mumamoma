514b8c0a964467ee5de186029a80e281d3d09be2
chan mi <mo0om487@naver.coml>
2022-11-24 04:21:27 +0900

    images 파일추가

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import MainPage from './components/MainPage';
 
 export default function App() {
