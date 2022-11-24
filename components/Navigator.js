30dc27c891a9371cc7a3b6ee38d02da471aadf0b
choizinyoung <ligt69209@gmail.com>
2022-11-24 16:38:46 +0900

    컴포넌트파일생성

---
+++ components/Navigator.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { DrawerNavigator } from "react-navigation";
+import InformationPage from "./InformationPage";
+import SettingPage from "./SettingPage";
+
+export default function Navigator() {
+  return (
+    <View style={styles.container}>
+      <Text>안녕하세요</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
