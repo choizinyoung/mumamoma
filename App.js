5eac58c397e0adfa6b40ffec9a22a5308f8b2977
choizinyoung <ligt69209@gmail.com>
2022-11-24 15:18:07 +0900

    필요없는파일삭제

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>mumamoma team</Text>
+      <Text>안녕 찬미야 민정아 테스트 성공</Text>
       <StatusBar style="auto" />
     </View>
   );
