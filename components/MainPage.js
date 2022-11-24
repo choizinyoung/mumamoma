174cc31398ab870e291c3c39f531f952c1328db3
summmermin <doniminiblog@gmail.com>
2022-11-24 17:42:18 +0900

    이미지추가

--- components/MainPage.js
+++ components/MainPage.js
@@ -1,11 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
 
 export default function MainPage() {
   return (
     <View style={styles.container}>
       <Text>안녕하세요</Text>
-      <Text>gpgpgpggmlglglg</Text>
+      <image
+        style={{ height: "100%", width: "100%", resizeMode: "contain" }}
+        source={require("./assets/images/cloud_main.png")}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +17,8 @@ export default function MainPage() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
