775266afe625a20355d280c8231d9c594ec87976
choizinyoung <ligt69209@gmail.com>
2022-11-24 15:41:44 +0900

    pull test

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>안녕 찬미야 민정아 테스트 성공</Text>
+      <Text>hey KCM Hi Yeah</Text>
       <StatusBar style="auto" />
     </View>
   );
