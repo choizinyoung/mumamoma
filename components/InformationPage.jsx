d0049283454ff0064bbc54d2e8a95349d1e91789
choizinyoung <ligt69209@gmail.com>
2022-12-02 00:39:29 +0900

    표 내용 추가

--- components/InformationPage.jsx
+++ components/InformationPage.jsx
@@ -1,38 +1,174 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View, Image, ActivityIndicator } from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, Text, View, Image, ActivityIndicator } from "react-native";
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <View style={styles.one}>
-        <View style={styles.whiteView}/>
-        <View style={styles.yellowView}>
-          <Image style={styles.container} source={require('../assets/images/Information_01.png')} resizeMode='cover'></Image>
-        </View>
-        <View style={styles.redView}>
-          <Image style={styles.container} source={require('../assets/images/Information_02.png')} resizeMode='cover'></Image>
-        </View>
-          <View style={styles.yellowView}>
-          <Image style={styles.container} source={require('../assets/images/Information_03.png')} resizeMode='cover'></Image>
-        </View>
-          <View style={styles.redView}>
-          <Image style={styles.container} source={require('../assets/images/Information_04.png')} resizeMode='cover'></Image>
-        </View>
+          <View style={[styles.whiteView, styles.width]} />
+          <View style={[styles.yellowView, styles.width]}>
+            <Image
+              style={styles.container}
+              source={require("../assets/images/Information_01.png")}
+              resizeMode="cover"
+            ></Image>
+          </View>
+          <View style={[styles.redView, styles.width]}>
+            <Image
+              style={styles.container}
+              source={require("../assets/images/Information_02.png")}
+              resizeMode="cover"
+            ></Image>
+          </View>
+          <View style={[styles.yellowView, styles.width]}>
+            <Image
+              style={styles.container}
+              source={require("../assets/images/Information_03.png")}
+              resizeMode="cover"
+            ></Image>
+          </View>
+          <View style={[styles.redView, styles.width]}>
+            <Image
+              style={styles.container}
+              source={require("../assets/images/Information_04.png")}
+              resizeMode="cover"
+            ></Image>
+          </View>
         </View>
         <View style={styles.two}>
-          <View style={styles.greenView} />
-          <View style={styles.Title1View}><Text>좋음</Text></View>
-          <View style={styles.Title2View}><Text>보통</Text></View>
-          <View style={styles.Title3View}><Text>나쁨</Text></View>
-          <View style={styles.Title4View}><Text>매우나쁨</Text></View>
+          <View style={[styles.greenView, styles.width]} />
+          <View style={[styles.Title1View, styles.width]}>
+            <Text>좋음</Text>
+          </View>
+          <View style={[styles.Title2View, styles.width]}>
+            <Text>보통</Text>
+          </View>
+          <View style={[styles.Title3View, styles.width]}>
+            <Text>나쁨</Text>
+          </View>
+          <View style={[styles.Title4View, styles.width]}>
+            <Text>매우나쁨</Text>
+          </View>
         </View>
         <View style={styles.third}>
-        <View style={styles.FirstView}><Text>통합지수</Text></View>
-        <View style={styles.PurpleView}><Text></Text></View>
-        <View style={styles.PurpleView}><Text></Text></View>
-        <View style={styles.PurpleView}><Text></Text></View>
-        <View style={styles.PurpleView}><Text></Text></View>
+          <View style={[styles.FirstView, styles.width1]}>
+            <Text>통합지수</Text>
+          </View>
+          <View style={[styles.GoodView, styles.width1]}>
+            <Text>0~50</Text>
+          </View>
+          <View style={[styles.NomalView, styles.width1]}>
+            <Text>51~100</Text>
+          </View>
+          <View style={[styles.BadView, styles.width1]}>
+            <Text>101~150</Text>
+          </View>
+          <View style={[styles.VeryView, styles.width1]}>
+            <Text>150~</Text>
+          </View>
+        </View>
+        <View style={styles.third}>
+          <View style={[styles.FirstView, styles.width1]}>
+            <Text>미세먼지</Text>
+          </View>
+          <View style={[styles.GoodView, styles.width1]}>
+            <Text>0~30</Text>
+          </View>
+          <View style={[styles.NomalView, styles.width1]}>
+            <Text>51~100</Text>
+          </View>
+          <View style={[styles.BadView, styles.width1]}>
+            <Text>101~150</Text>
+          </View>
+          <View style={[styles.VeryView, styles.width1]}>
+            <Text>150~</Text>
+          </View>
+        </View>
+        <View style={styles.third}>
+          <View style={[styles.FirstView, styles.width1]}>
+            <Text>초미세먼지</Text>
+          </View>
+          <View style={[styles.GoodView, styles.width1]}>
+            <Text>0~15</Text>
+          </View>
+          <View style={[styles.NomalView, styles.width1]}>
+            <Text>51~100</Text>
+          </View>
+          <View style={[styles.BadView, styles.width1]}>
+            <Text>101~150</Text>
+          </View>
+          <View style={[styles.VeryView, styles.width1]}>
+            <Text>150~</Text>
+          </View>
+        </View>
+        <View style={styles.third}>
+          <View style={[styles.FirstView, styles.width1]}>
+            <Text>오존</Text>
+          </View>
+          <View style={[styles.GoodView, styles.width1]}>
+            <Text>0~0.03</Text>
+          </View>
+          <View style={[styles.NomalView, styles.width1]}>
+            <Text>51~100</Text>
+          </View>
+          <View style={[styles.BadView, styles.width1]}>
+            <Text>101~150</Text>
+          </View>
+          <View style={[styles.VeryView, styles.width1]}>
+            <Text>150~</Text>
+          </View>
+        </View>
+        <View style={styles.third}>
+          <View style={[styles.FirstView, styles.width1]}>
+            <Text>이산화질소</Text>
+          </View>
+          <View style={[styles.GoodView, styles.width1]}>
+            <Text>0~0.03</Text>
+          </View>
+          <View style={[styles.NomalView, styles.width1]}>
+            <Text>51~100</Text>
+          </View>
+          <View style={[styles.BadView, styles.width1]}>
+            <Text>101~150</Text>
+          </View>
+          <View style={[styles.VeryView, styles.width1]}>
+            <Text>150~</Text>
+          </View>
+        </View>
+        <View style={styles.third}>
+          <View style={[styles.FirstView, styles.width1]}>
+            <Text>일산화질소</Text>
+          </View>
+          <View style={[styles.GoodView, styles.width1]}>
+            <Text>0~2</Text>
+          </View>
+          <View style={[styles.NomalView, styles.width1]}>
+            <Text>51~100</Text>
+          </View>
+          <View style={[styles.BadView, styles.width1]}>
+            <Text>101~150</Text>
+          </View>
+          <View style={[styles.VeryView, styles.width1]}>
+            <Text>150~</Text>
+          </View>
+        </View>
+        <View style={styles.third}>
+          <View style={[styles.FirstView, styles.width1]}>
+            <Text>아황산가스</Text>
+          </View>
+          <View style={[styles.GoodView, styles.width1]}>
+            <Text>0~2</Text>
+          </View>
+          <View style={[styles.NomalView, styles.width1]}>
+            <Text>51~100</Text>
+          </View>
+          <View style={[styles.BadView, styles.width1]}>
+            <Text>101~150</Text>
+          </View>
+          <View style={[styles.VeryView, styles.width1]}>
+            <Text>150~</Text>
+          </View>
         </View>
       </View>
     );
@@ -41,71 +177,78 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    textAlign: 'center',
+    backgroundColor: "#fff",
+    textAlign: "center",
+    alignItems: "center",
   },
   one: {
-    justifyContent: 'flex-start',
-    flexDirection: 'row',
-    flexWrap: 'wrap',
+    justifyContent: "flex-start",
+    flexDirection: "row",
+    flexWrap: "wrap",
   },
   whiteView: {
     height: 72,
-    width: 72,
-    backgroundColor: 'white',
+    backgroundColor: "white",
   },
   redView: {
     height: 72,
-    width: 72,
-    backgroundColor: 'red',
+    backgroundColor: "red",
   },
   yellowView: {
     height: 72,
-    width: 72,
-    backgroundColor: 'yellow',
+    backgroundColor: "yellow",
   },
   two: {
-    justifyContent: 'flex-start',
-    flexDirection: 'row',
-    flexWrap: 'wrap',
+    justifyContent: "flex-start",
+    flexDirection: "row",
+    flexWrap: "wrap",
   },
   greenView: {
     height: 32,
-    width: 72,
-    backgroundColor: 'white',
+    backgroundColor: "white",
   },
   Title1View: {
     height: 32,
-    width: 72,
-    backgroundColor: '#AEEDFF',
+    backgroundColor: "#AEEDFF",
   },
   Title2View: {
     height: 32,
-    width: 72,
-    backgroundColor: '#CDFFAE',
+    backgroundColor: "#CDFFAE",
   },
   Title3View: {
     height: 32,
-    width: 72,
-    backgroundColor: '#FFDFAE',
+    backgroundColor: "#FFDFAE",
   },
   Title4View: {
     height: 32,
-    width: 72,
-    backgroundColor: '#FFAEAE',
+    backgroundColor: "#FFAEAE",
   },
-  third:{
-    justifyContent: 'flex-start',
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-  },
-  PurpleView:{
-    height: 40,
-    width: 72,
-    backgroundColor: '#CEF4FF',
-  },
-  FirstView:{
-    borderBottomColor: 'black',
+  third: {
+    justifyContent: "flex-start",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    borderBottomColor: "black",
     borderBottomWidth: 1,
-  }
+  },
+  GoodView: {
+    backgroundColor: "#CEF4FF",
+  },
+  NomalView: {
+    backgroundColor: "#E1FFCE",
+  },
+  BadView: {
+    backgroundColor: "#FFECCE",
+  },
+  VeryView: {
+    backgroundColor: "#FFCECE",
+  },
+  width : {
+    width : 72,
+    justifyContent: "center",
+  },
+  width1 : {
+    width : 72,
+    height: 40,
+    justifyContent: "center",
+  },
 });
