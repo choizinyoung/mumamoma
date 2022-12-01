9d23506d489a6b99a8e790b22986fb0e6c37c1cf
chan mi <mo0om487@naver.coml>
2022-12-01 18:26:55 +0900

    이미지표추가

--- components/MainPage.jsx
+++ components/MainPage.jsx
@@ -9,25 +9,19 @@ import {
   Image,
 } from "react-native";
 import MainImage from "../assets/images/cloud_main.png";
-<<<<<<< HEAD
-=======
 import MainImageB from "../assets/images/cloudb.png";
->>>>>>> 49743464e73e01971ba9c933078f52a4d5a6fa96
 
 export default function MainPage({ navigation }) {
   return (
     <View style={styles.container}>
-<<<<<<< HEAD
       <Text>메인페이지</Text>
       <StatusBar style="auto" />
       <Image source={MainImage} style={Mainimgstyles.image} />
-=======
       <Text>강남구 일원동</Text>
       <Text>날짜 시간 </Text>
       <StatusBar style="auto" />
       <Image source={MainImage} style={Mainimgstyles.image} />
       <Image source={MainImageB} style={MainimgstylesB.image} />
->>>>>>> 49743464e73e01971ba9c933078f52a4d5a6fa96
     </View>
   );
 }
@@ -40,8 +34,6 @@ const styles = StyleSheet.create({
   },
 });
 
-<<<<<<< HEAD
-=======
 const MainimgstylesB = StyleSheet.create({
   image: {
     width: "100%",
@@ -49,18 +41,12 @@ const MainimgstylesB = StyleSheet.create({
     resizeMode: "cover",
   },
 });
->>>>>>> 49743464e73e01971ba9c933078f52a4d5a6fa96
 const Mainimgstyles = StyleSheet.create({
   image: {
     width: "70%",
     height: "70%",
     resizeMode: "contain",
-<<<<<<< HEAD
-  },
-});
-=======
     flexDirection: "column",
     marginBottom: -300,
   },
 });
->>>>>>> 49743464e73e01971ba9c933078f52a4d5a6fa96
