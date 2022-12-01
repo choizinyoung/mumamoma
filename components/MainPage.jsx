067842b813d1df7d5273c3804e32abcf1fdf375e
summmermin <doniminiblog@gmail.com>
2022-12-01 14:16:58 +0900

    메인페이지 이미지 추가

--- components/MainPage.jsx
+++ components/MainPage.jsx
@@ -1,11 +1,21 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Button } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  ActivityIndicator,
+  Image,
+} from "react-native";
+import MainImage from "../assets/images/cloud_main.png";
 
 export default function MainPage({ navigation }) {
   return (
     <View style={styles.container}>
       <Text>MainPage</Text>
-      <StatusBar style="auto"/>
+      <StatusBar style="auto" />
+      <Image source={MainImage} style={Mainimgstyles.image} />
     </View>
   );
 }
@@ -15,5 +25,13 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+});
+
+const Mainimgstyles = StyleSheet.create({
+  image: {
+    width: "70%",
+    height: "70%",
+    resizeMode: "contain",
   },
 });
